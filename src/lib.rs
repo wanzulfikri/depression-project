7a5b6606596fb23f93694515ad2ef1eaaafe95a5
wanzulfikri <wanzulfikri@gmail.com>
2022-02-02 12:52:40 +0800

    Add constructor for Activity

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 pub fn run() {
     println!("Running...");
-    let new_activity = Activity {
-        name: String::from("new"),
-    };
+    let new_activity = Activity::new(&String::from("new"));
     println!("{:?}", new_activity);
 }
 
 #[derive(Debug)]
 struct Activity {
     name: String,
+}
+
+impl Activity {
+    fn new(name: &String) -> Activity {
+        let name = name.clone();
+        Activity { name }
+    }
 }
