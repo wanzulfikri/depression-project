62b82987f0a8de7f03a1147fccc0d748fd39b0d5
wanzulfikri <wanzulfikri@gmail.com>
2022-02-08 14:18:00 +0800

    Improve error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
 use std::fs;
 use std::fs::File;
-use std::io;
 
+use std::io;
 use std::io::Write;
+
+use std::error::Error;
 
 use serde::{Deserialize, Serialize};
 
 // TODO: Add choices
 // TODO: Once the above is done, use Result and add tests
 
-pub fn run() {
-    println!("Running depression_project_cli");
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let current_activity = match deserialize_to_activity("activity.json") {
+        Ok(activity) => activity,
+        Err(error) => return Err(error),
+    };
     println!("Current activity:");
-    println!("{:?}", deserialize_to_activity());
+
+    println!("{:?}", current_activity);
     print!("Enter activity name: ");
     io::stdout().flush().unwrap();
 
@@ -25,29 +31,34 @@ pub fn run() {
     let new_activity = Activity::new(&name);
     println!("{:?}", new_activity);
 
-    let serialized_activity = serialize_to_json(&new_activity);
+    let serialized_activity = match serialize_to_json(&new_activity) {
+        Ok(activity) => activity,
+        Err(error) => return Err(error),
+    };
     println!("{}", serialized_activity);
 
     save_string_to_file(&serialized_activity);
+
+    Ok(())
 }
 
 // TODO: replace unwrap with proper Result
-fn serialize_to_json(activity: &Activity) -> String {
+fn serialize_to_json(activity: &Activity) -> Result<String, Box<dyn Error>> {
     let serialized_activity = serde_json::to_string(activity).unwrap();
-    serialized_activity
+    Ok(serialized_activity)
 }
 
 // TODO: deserialize
-fn deserialize_to_activity() -> Activity {
-    let content = fs::read_to_string("test.txt").expect("File not read");
-    let deserialized_activity: Activity = serde_json::from_str(&content).unwrap();
-    deserialized_activity
+fn deserialize_to_activity(filename: &str) -> Result<Activity, Box<dyn Error>> {
+    let content = fs::read_to_string(filename)?;
+    let deserialized_activity: Activity = serde_json::from_str(&content)?;
+    Ok(deserialized_activity)
 }
 
 // TODO: save to file
 
 fn save_string_to_file(content: &String) {
-    let mut file = File::create("test.txt").expect("File cannot be created");
+    let mut file = File::create("activity.json").expect("File cannot be created");
     file.write_all(content.as_bytes())
         .expect("File cannot be written");
 }
@@ -69,3 +80,5 @@ impl Activity {
         Activity { name }
     }
 }
+
+// TODO: Add tests for available and unavailable files
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use dp_lib::run;
 fn main() {
-    run();
+    println!("Running depression_project_cli");
+    match run() {
+        Ok(_) => println!(""),
+        // TODO: might want to use Custom Error types instead for more flexibility
+        Err(error) => println!("{:?}", error),
+    }
 }
